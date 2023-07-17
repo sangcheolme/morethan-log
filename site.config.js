e96d69c6395e1afb3b6f6f4345a4aacbed394264
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2023-07-18 07:38:20 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "sc-dev",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/new-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I develop everything using java spring.",
     email: "scpark0698@gmail.com",
