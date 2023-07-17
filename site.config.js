f5ceeb680274d07289df040b540b8f5e9010b6fd
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2023-07-18 05:07:26 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "sc-dev",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "backend developer",
+    bio: "I develop everything using java spring.",
+    email: "scpark0698@gmail.com",
+    linkedin: "",
+    github: "sangcheolme",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `github`,
+      href: "https://https://github.com/sangcheolme",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "sc-dev",
+    description: "welcome to my-blog!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
